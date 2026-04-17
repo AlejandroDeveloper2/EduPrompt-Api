d2bd1cc6affe2f4930e1047464ba9cfea2163a14
Diego Alejandro Diaz <90485793+AlejandroDeveloper2@users.noreply.github.com>
2026-04-17 09:45:14 -0500

    fix(dto): update validation to ensure non-negative values for resources and tokens

--- src/features/indicators/application/dto/createIndicators.dto.ts
+++ src/features/indicators/application/dto/createIndicators.dto.ts
@@ -2,11 +2,11 @@ import { z } from "zod/v4";
 
 /** Dto de entrada */
 export const CreateIndicatorsDto = z.object({
-  generatedResources: z.number().positive(),
-  usedTokens: z.number().positive(),
+  generatedResources: z.number().min(0),
+  usedTokens: z.number().min(0),
   lastGeneratedResource: z.string().nullable(),
-  dowloadedResources: z.number().positive(),
-  savedResources: z.number().positive(),
+  dowloadedResources: z.number().min(0),
+  savedResources: z.number().min(0),
 });
 
 /** Tipos de entrada */
